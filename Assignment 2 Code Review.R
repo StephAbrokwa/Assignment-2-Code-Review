b47eb2bde81151c50d21f9de278a9c7e180246b7
Steph Abro <stephabro@Stephs-MacBook-Pro.local>
2023-06-16 17:59:36 -0400

    commit

--- Assignment 2 Code Review.R
+++ Assignment 2 Code Review.R
@@ -1,1 +1,11 @@
 # Assignment 2 Code Review 
+
+#Assignment Guidelines
+    # They successfully printed a message to prompt the user to enter a three digit positive number 
+    # They successfully ensured that the user input 
+
+#Specific Code Line Comments 
+
+#Suggestions for Improvement 
+
+#Overall Opinion
